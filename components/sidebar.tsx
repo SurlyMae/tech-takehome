45a1ec0be5c969c078bd027118d193b1c53d0a8f
Ashly LaMarr <surlymae@gmail.com>
2022-07-25 14:09:34 -0600

    Frame out sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -24,7 +24,7 @@ const navMenu = [
     route: "/",
   },
   {
-    label: "Your Events",
+    label: "Events",
     icon: MdCalendarToday,
     route: "/events",
   },
@@ -41,6 +41,24 @@ const createEvent = {
   route: "/",
 };
 
+const events = [
+  {
+    id: 1,
+    label: "Rowing Camp",
+    route: "/",
+  },
+  {
+    id: 2,
+    label: "Marina BBQ",
+    route: "/",
+  },
+  {
+    id: 3,
+    label: "Boatyard Cleanup",
+    route: "/",
+  },
+];
+
 const Sidebar = () => {
   return (
     <Box
@@ -50,7 +68,7 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
@@ -87,7 +105,20 @@ const Sidebar = () => {
               </LinkBox>
             </ListItem>
           </List>
-          <Divider />
+          <Divider color="gray" />
+          <Box height="66%" overflowY="auto" paddingY="20px">
+            <List spacing={2}>
+              {events.map((event) => (
+                <ListItem paddingX="20px" key={event.id}>
+                  <LinkBox>
+                    <NextLink href={event.route} passHref>
+                      <LinkOverlay>{event.label}</LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Box>
       </Box>
     </Box>
